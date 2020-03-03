7b387fe496dcf6b78f2893d2595514ccdd5c2b9a
Jake Sanders <jakaria9@gmail.com>
2020-03-03 17:45:19 -0500

    initial LoadData added

---
+++ core/src/edu/cornell/gdiac/physics/Level.java
@@ -0,0 +1,54 @@
+package edu.cornell.gdiac.physics;
+
+import com.badlogic.gdx.math.Vector2;
+import edu.cornell.gdiac.physics.obstacle.Obstacle;
+import edu.cornell.gdiac.physics.robot.RobotModel;
+
+/**
+ * A simple struct-like class that holds the elements of a level.
+ */
+public class Level {
+
+    /**
+     * Constructs a simple object encapsulating the elements of a level
+     *
+     * @param regions A 2D array, where each element of the inner array
+     *                      represents an "island" in the board. That island is
+     *                      represented as an array of coordinates
+     * @param obstacles An array of the obstacles in the level
+     * @param robots An array of the robots in the level
+     * @param start The "robot" where the player starts
+     */
+    public Level(
+            Vector2[][] regions,
+            Obstacle[] obstacles,
+            RobotModel[] robots,
+            RobotModel start
+    ) {
+       this.regions = regions;
+       this.obstacles = obstacles;
+       this.robots = robots;
+       this.start = start;
+    }
+
+    /**
+     * An array of regions on the board. Each region is an island within which
+     * robots can be located. Outside each counts as out of bounds.
+     *
+     * Each region is represented as a list of points being the polygon
+     * borders of the region.
+     *
+     * Example: A single square region may be represented as
+     * [ [ (0, 0), (0, 100), (100, 100), (100, 0) ] ]
+     */
+    public Vector2[][] regions;
+
+    /** An array of the obstacles in the level */
+    public Obstacle[] obstacles;
+
+    /** An array of the robots in the level */
+    public RobotModel[] robots;
+
+    /** The "robot" where the player starts */
+    public RobotModel start;
+}
