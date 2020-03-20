323d02631e08403e2133473cd4ce0f8287048d5f
May Chen <mc2249@cornell.edu>
2020-03-19 23:01:08 -0400

    arrow class as an obstacle

--- core/src/edu/cornell/gdiac/physics/robot/ArrowModel.java
+++ core/src/edu/cornell/gdiac/physics/robot/ArrowModel.java
@@ -1,35 +1,59 @@
-package edu.cornell.gdiac.physics.spirit;
+package edu.cornell.gdiac.physics.robot;
 
-import edu.cornell.gdiac.physics.obstacle.BoxObstacle;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector;
+import com.badlogic.gdx.math.Vector2;
+import edu.cornell.gdiac.physics.GameCanvas;
 
 
-public class SpiritModel extends BoxObstacle {
+public class ArrowModel {
+    private Vector2 start;
+    private Vector2 offset;
+    private Vector2 prevLoc;
+    private Vector2 currLoc;
+    private float width;
+    private float height;
+    private float sx;
+    private float sy;
+    private float ang;
 
-    public int bounces;
+    Texture arrTexture;
 
-    public SpiritModel(float x, float y) {
-        super(x, y, 10, 10);
-        bounces = 4;
+    public ArrowModel(Texture arrText, Vector2 clickPosStart, Vector2 robPos) {
+        this.arrTexture = arrText;
+        start = robPos;
+        this.width = width;
+        this.height = height;
+        offset = new Vector2(0,0);
+        ang = robPos.sub(clickPosStart).angleRad() ;
+        sx = 1;
+        sy = 1;
+        prevLoc = null;
+        currLoc = new Vector2(start);
     }
 
-    public SpiritModel(float x, float y, int b) {
-        super(x, y, 10, 10);
-        bounces = b;
+    public void setScale(Vector2 pos){
+        prevLoc = currLoc;
+        currLoc = pos;
+        Vector2 diffCurr =  new Vector2(currLoc).sub(new Vector2((start)));
+
+//        if (pos.y > start.y || pos.x > start.x){
+//            sy = sy + .01f;
+//        }
+//        else if (pos.y < start.y || pos.x < start.x){
+//            sy = sy - .01f;
+//        }
+        System.out.println(diffCurr);
+        //System.out.println(diffCurr.angle());
+        ang = -6.28f + diffCurr.angleRad();
+        offset = diffCurr.setLength(75f);
     }
 
-    public SpiritModel(float x, float y, float width, float height, int b) {
-        super(x, y, width, height);
-        bounces = b;
-    }
-
-
-
-    public boolean decBounces(){
-        if(bounces == 0){
-            return false;
-        }else{
-            bounces--;
-            return true;
-        }
+    public void draw (GameCanvas canvas) {
+        canvas.begin();
+        canvas.draw(arrTexture, Color.PURPLE, arrTexture.getWidth()/2, arrTexture.getHeight()/2, start.x - offset.x, start.y - offset.y,  ang, sx, sy);
+        canvas.end();
     }
 }
