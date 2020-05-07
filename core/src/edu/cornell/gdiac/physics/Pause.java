dfc1e6d2c35000e09e6d7ed241eec65da1527eba
Stacy <sw653@cornell.edu>
2020-05-07 18:05:46 -0500

    Fix shooting bug

--- core/src/edu/cornell/gdiac/physics/Pause.java
+++ core/src/edu/cornell/gdiac/physics/Pause.java
@@ -3,7 +3,6 @@ package edu.cornell.gdiac.physics;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
