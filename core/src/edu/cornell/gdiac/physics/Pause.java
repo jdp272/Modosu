5a3beb4d3af60b6949a601bf6c26a971763ecacc
Stacy <sw653@cornell.edu>
2020-05-07 18:45:45 -0500

    jk now fixed the sbug

--- core/src/edu/cornell/gdiac/physics/Pause.java
+++ core/src/edu/cornell/gdiac/physics/Pause.java
@@ -14,7 +14,6 @@ import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import edu.cornell.gdiac.util.MusicController;
