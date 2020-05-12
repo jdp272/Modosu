067263e594bea15865b66d31a07f90e185183188
AidanHobler <ajh343@cornell.edu>
2020-05-12 04:55:15 -0400

    Fixing camera problems

--- core/src/edu/cornell/gdiac/physics/GamePlayController.java
+++ core/src/edu/cornell/gdiac/physics/GamePlayController.java
@@ -395,7 +395,8 @@ public class GamePlayController extends WorldController {
 
 		// Calculate spirit's screen coordinates from box2d coordinates
 		if (possessed.isPedestal() && !spirit.hasLaunched){
-
+			System.out.println(panTarget);
+			System.out.println(scale);
 			if (InputController.getInstance().didTertiary()) {
 				panTarget = pedestal.getPosition();
 				panTarget.x *= scale.x;
@@ -407,6 +408,7 @@ public class GamePlayController extends WorldController {
 		    cache.set(panTarget);
 		}
 		else {
+			System.out.println("Not panning");
 			cache.set(spirit.getPosition());
 			cache.scl(scale.x, scale.y);
 		}
